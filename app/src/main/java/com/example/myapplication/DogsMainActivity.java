b63d78417394b6a34188ade7ac90ac341b77e89b
Kyulang <rbfxod1223@gmail.com>
2022-01-12 12:08:27 +0900

    like button commit

--- app/src/main/java/com/example/myapplication/DogsMainActivity.java
+++ app/src/main/java/com/example/myapplication/DogsMainActivity.java
@@ -5,6 +5,8 @@ import static android.view.View.*;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -13,6 +15,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class DogsMainActivity extends AppCompatActivity {
+    String val = "";
     private ImageButton imgBtn_search;
     private EditText editTxt_search;
     private String search;
@@ -41,7 +44,7 @@ public class DogsMainActivity extends AppCompatActivity {
         imgBtn_home.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Intent intent = new Intent(DogsMainActivity.this, MainActivity.class);
+                Intent intent = new Intent(DogsMainActivity.this, TypeActivity.class);
                 startActivity(intent);  // activity 이동
             }
         });
@@ -54,5 +57,22 @@ public class DogsMainActivity extends AppCompatActivity {
             }
         });
 
+//    25~마지막줄은 수정해야 함. 미완성 레시피 추천 함수임.
+//            public void getVal(); {
+//
+//                DatabaseHelper dbHelper = new DatabaseHelper(this);
+//                SQLiteDatabase db = dbHelper.getReadableDatabase();
+//
+//                Cursor cursor = db.rawQuery("SELECT * FROM recipe_ingredient where RECIPE_ID = 181",null);
+//                //" and name = ?",new String[]{"홍길동"});
+//                while (cursor.moveToNext())
+//                {
+//                    val += cursor.getString(2)+", ";
+//
+//                }
+//                sqlresult.setText("재료: "+val);
+//                cursor.close();
+//                dbHelper.close();
+//            }
     }
 }
