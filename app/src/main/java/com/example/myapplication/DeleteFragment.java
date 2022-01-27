14fc760885cb800b6c3256527e847f510cee0ce2
hagummy <haky619@naver.com>
2022-01-27 17:21:41 +0900

    회원 db,auth 탈퇴

--- app/src/main/java/com/example/myapplication/DeleteFragment.java
+++ app/src/main/java/com/example/myapplication/DeleteFragment.java
@@ -4,16 +4,25 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class DeleteFragment extends Fragment {
     ViewGroup rootView;
@@ -23,6 +32,8 @@ public class DeleteFragment extends Fragment {
 
     private Button btn_yes;
     private FirebaseAuth firebaseAuth;
+    private DatabaseReference reference;
+    private FirebaseDatabase database;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -39,12 +50,19 @@ public class DeleteFragment extends Fragment {
         firebaseAuth = FirebaseAuth.getInstance();
         FirebaseUser user = firebaseAuth.getCurrentUser();
 
+
+        reference = FirebaseDatabase.getInstance().getReference("Cookforpet");
         btn_yes = rootView.findViewById(R.id.btn_yes);
         btn_yes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                reference.child("UserAccount").child(user.getUid()).removeValue();
+
                 user.delete();
+
                 Intent intent = new Intent(context, MainActivity.class);
+
                 startActivity(intent);  // activity 이동
             }
         });
